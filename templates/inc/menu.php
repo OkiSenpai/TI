b0bb4be8fa8da6e45e83f423a46451aa6479284e
OkiSenpai <omersatelit@gmail.com>
2025-02-19 09:50:35 +0100

    nav added

--- templates/inc/menu.php
+++ templates/inc/menu.php
@@ -1,10 +1,11 @@
 <ul>
     <li><a href="./">Accueil</a></li>
-    <li><a href="./?p=geographie"></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
-    <li><a href=""></a></li>
+    <li><a href="./?p=geographie">Geographie</a></li>
+    <li><a href="./?p=histoire">Histoire</a></li>
+    <li><a href="./?p=culture">Culture</a></li>
+    <li><a href="./?p=galerie">Glerie</a></li>
+    <li><a href="./?p=contact">Contact</a></li>
+    <li><a href="./?p=liens">liens</a></li>
+    <li><a href="./?p=page-404">Page-404</a></li>
+
 </ul>
