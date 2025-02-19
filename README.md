41fe23a29a49640626f0bb4bfc5b81d30337a5d6
OkiSenpai <omersatelit@gmail.com>
2025-02-19 12:08:51 +0100

    README chenged, added more details about what am I doing

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# TI
-# TI
+# 🌍 T.I.1 – Réalisation d’un site statique en PHP (2025)
+
+Ce projet est réalisé dans le cadre du test d'intégration (T.I.1) et consiste en la création d'un site web statique présentant une capitale européenne. L'objectif est d'appliquer le modèle MVC en PHP.
+
+## 📌 Objectif
+
+Le but est de développer un site web statique en utilisant PHP et une architecture MVC simple. Un exemple basé sur la ville de Bruxelles est fourni comme référence.
+
+## 📜 Consignes
+
+- Le site comprend **7 pages principales** (+ une page d'erreur 404).
+- Utilisation d'un **contrôleur frontal** pour gérer la navigation.
+- Respect des **URLs prédéfinies**.
+
+## 🛠️ Technologies utilisées
+
+- PHP
+- HTML/CSS
+- MVC (architecture simplifiée)
+
+## 🚀 Installation
+
+1. Cloner le projet :
+   ```bash
+   git clone https://github.com/OkiSenpai/nom-du-repo.git
+   ```
--- templates/accueil.php
+++ templates/accueil.php
@@ -18,97 +18,74 @@
 
 
     </header>
-    <div class="sticky">I am sticky!</div>
+    <main>
+        <div class="sticky">I am sticky!</div>
+        <h2>Bienvenue sur notre site consacré a Amsterdam</h2>
+        <img src="img/armoiries.png" alt="armoiries.png">
 
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-    </p>
-    <>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus molestias.
-        Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus eos.
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
+        <p>Amsterdam est la capitale des Pays-Bas, bien que le gouvernement ainsi que la plupart des institutions
+            nationales
+            siègent à La Haye. Sur la base des chiffres de l'année 2017, la commune d'Amsterdam compte plus de 850 000
+            habitants, appelés Amstellodamois, ce qui en fait la commune néerlandaise la plus peuplée. Elle est située
+            au
+            cœur de la région d'Amsterdam, regroupant environ 1 350 000 habitants. L'aire urbaine, qui rassemble plus de
+            2
+            400 000 résidents fait elle-même partie d'une conurbation appelée Randstad qui compte 7 100 000 habitants.
+            La
+            ville est la plus grande de Hollande-Septentrionale, mais n'est cependant pas le chef-lieu de la province,
+            ce
+            dernier étant Haarlem, situé à 19 kilomètres à l'ouest d'Amsterdam.
         </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
+
+
+        <p>Amsterdam est la capitale des Pays-Bas, bien que le gouvernement ainsi que la plupart des institutions
+            nationales
+            siègent à La Haye. Sur la base des chiffres de l'année 2017, la commune d'Amsterdam compte plus de 850 000
+            habitants, appelés Amstellodamois, ce qui en fait la commune néerlandaise la plus peuplée. Elle est située
+            au
+            cœur de la région d'Amsterdam, regroupant environ 1 350 000 habitants. L'aire urbaine, qui rassemble plus de
+            2
+            400 000 résidents fait elle-même partie d'une conurbation appelée Randstad qui compte 7 100 000 habitants.
+            La
+            ville est la plus grande de Hollande-Septentrionale, mais n'est cependant pas le chef-lieu de la province,
+            ce
+            dernier étant Haarlem, situé à 19 kilomètres à l'ouest d'Amsterdam.
         </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
+
+
+        <p>Amsterdam est la capitale des Pays-Bas, bien que le gouvernement ainsi que la plupart des institutions
+            nationales
+            siègent à La Haye. Sur la base des chiffres de l'année 2017, la commune d'Amsterdam compte plus de 850 000
+            habitants, appelés Amstellodamois, ce qui en fait la commune néerlandaise la plus peuplée. Elle est située
+            au
+            cœur de la région d'Amsterdam, regroupant environ 1 350 000 habitants. L'aire urbaine, qui rassemble plus de
+            2
+            400 000 résidents fait elle-même partie d'une conurbation appelée Randstad qui compte 7 100 000 habitants.
+            La
+            ville est la plus grande de Hollande-Septentrionale, mais n'est cependant pas le chef-lieu de la province,
+            ce
+            dernier étant Haarlem, situé à 19 kilomètres à l'ouest d'Amsterdam.
         </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
+
+        <p>Amsterdam est la capitale des Pays-Bas, bien que le gouvernement ainsi que la plupart des institutions
+            nationales
+            siègent à La Haye. Sur la base des chiffres de l'année 2017, la commune d'Amsterdam compte plus de 850 000
+            habitants, appelés Amstellodamois, ce qui en fait la commune néerlandaise la plus peuplée. Elle est située
+            au
+            cœur de la région d'Amsterdam, regroupant environ 1 350 000 habitants. L'aire urbaine, qui rassemble plus de
+            2
+            400 000 résidents fait elle-même partie d'une conurbation appelée Randstad qui compte 7 100 000 habitants.
+            La
+            ville est la plus grande de Hollande-Septentrionale, mais n'est cependant pas le chef-lieu de la province,
+            ce
+            dernier étant Haarlem, situé à 19 kilomètres à l'ouest d'Amsterdam.
         </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure culpa adipisci, optio facilis eos natus
-            molestias.
-            Alias a voluptatem, magnam magni enim, saepe dolorem molestias repudiandae, dolore consectetur doloribus
-            eos.
-        </p>
+
+
+
+
+    </main>
+
 
 
 </body>
