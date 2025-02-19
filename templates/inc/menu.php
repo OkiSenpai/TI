943e809358c8d0d8d1ffe8e6e05076614565d407
OkiSenpai <omersatelit@gmail.com>
2025-02-19 09:48:40 +0100

    screenshots added

---
+++ templates/inc/menu.php
@@ -0,0 +1,10 @@
+<ul>
+    <li><a href="./">Accueil</a></li>
+    <li><a href="./?p=geographie"></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul>
