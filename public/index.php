4f1a29394210e8332b292b5c8da4edb1645fdebc
OkiSenpai <omersatelit@gmail.com>
2025-02-20 09:27:29 +0100

    title added by php

--- public/index.php
+++ public/index.php
@@ -4,26 +4,35 @@ if (isset($_GET["p"])) {
     switch ($_GET["p"]) {
         case "geographie":
             include '../templates/geographie.php';
+            $title = "Géographie";
             break;
         case 'histoire':
             include '../templates/histoire.php';
+            $title = 'Histoire';
             break;
         case 'culture':
             include '../templates/culture.php';
+            $title = 'Culture';
             break;
         case 'galerie':
             include '../templates/galerie.php';
+            $title = 'Galerie';
             break;
         case 'contact':
             include '../templates/contact.php';
+            $title = 'Contact';
             break;
         case 'liens':
             include '../templates/liens.php';
+            $title = 'Liens';
             break;
         default:
             include '../templates/page-404.php';
+            $title = 'Erreur 404';
             break;
     }
 } else {
+    $title = 'Accueil';
+
     include '../templates/accueil.php';
 }
